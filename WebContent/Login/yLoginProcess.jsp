b6c1c4e62f95f1132a300f6bcc9ef85733173da8
zo_teeng <te9908@naver.com>
2023-06-25 04:11:40 +0900

    06250411 teeng

--- WebContent/Login/yLoginProcess.jsp
+++ WebContent/Login/yLoginProcess.jsp
@@ -11,21 +11,21 @@ String userPwd = request.getParameter("user_pw");
 // 회원 테이블 DAO를 통해 회원 정보 DTO 획득
 // MemberDAO dao = new MemberDAO(oracleDriver, oracleURL, oracleId, oraclePwd);
 memberDAO dao = new memberDAO(application);
-memberDTO memberDTO = dao.getMemberDTO(userId, userPwd);
+memberDTO memberDTO = dao.getMemberDTO(userId);
 dao.close();
 
 // 로그인 성공 여부에 따른 처리
 if(memberDTO.getId() != null){
-	// 로그인 성공
-	session.setAttribute("UserId", userId);
-	session.setAttribute("UserPw", userPwd);
-	session.setAttribute("UserName", memberDTO.getName());
-	JSFunction.alertLocation("로그인 되었습니다.", "../Main/Main.jsp" , out);
-	// response.sendRedirect("../Main/Main.jsp");
+   // 로그인 성공
+   session.setAttribute("UserId", userId);
+   session.setAttribute("UserPw", userPwd);
+   session.setAttribute("UserName", memberDTO.getName());
+   JSFunction.alertLocation("로그인 되었습니다.", "../Main/Main.jsp" , out);
+   // response.sendRedirect("../Main/Main.jsp");
 }
 else{
-	// 로그인 실패
-	request.setAttribute("LoginErrMsg", "로그인 오류입니다.");
-	request.getRequestDispatcher("yLoginForm.jsp").forward(request, response);
+   // 로그인 실패
+   request.setAttribute("LoginErrMsg", "로그인 오류입니다.");
+   request.getRequestDispatcher("yLoginForm.jsp").forward(request, response);
 }
 %>
--- src/member/memberDAO.java
+++ src/member/memberDAO.java
@@ -11,186 +11,183 @@ import javax.servlet.ServletContext;
 import member.memberDTO;
 
 public class memberDAO extends JDBConnect  {
-	public memberDAO(ServletContext application)
-	{
-		super(application);
-	}
-	public memberDTO getMemberDTO(String id, String pw){ //데이터 검색
-		memberDTO dto=new memberDTO();
-		String sql="select * from member where id=? and pass=?";
+   public memberDAO(ServletContext application)
+   {
+      super(application);
+   }
+   public memberDTO getMemberDTO(String id){ //데이터 검색
+      memberDTO dto=new memberDTO();
+      String sql="select * from member where id=?";
       try{
-			psmt=con.prepareStatement(sql);
-			psmt.setString(1, id);
-			psmt.setString(2, pw);
-			rs=psmt.executeQuery();
-			if(rs.next())
-			{
-				dto.setId(rs.getString(1));
-				dto.setPass(rs.getString(2));
-				dto.setNickname(rs.getString(3));
-				dto.setName(rs.getString(4));
-				dto.setTel(rs.getString(5));
-				dto.setRegidate(rs.getDate(6));
+         psmt=con.prepareStatement(sql);
+         psmt.setString(1, id);
+         rs=psmt.executeQuery();
+         if(rs.next())
+         {
+            dto.setId(rs.getString(1));
+            dto.setPass(rs.getString(2));
+            dto.setName(rs.getString(3));
+            dto.setTel(rs.getString(4));
+            dto.setNickname(rs.getString(5));
+            dto.setRegidate(rs.getDate(6));
 
-			}
-		}catch(Exception e)
-		{
-			e.printStackTrace();
-		}
+         }
+      }catch(Exception e)
+      {
+         e.printStackTrace();
+      }
 
-		return dto;
+      return dto;
 
-	}
+   }
 
-	public boolean checkId(String id) {
-		boolean result=true;
-		memberDTO dto=new memberDTO();
-		try {
-			String sql = "select id from member where id=?";
-			psmt=con.prepareStatement(sql);
-			psmt.setString(1, id);
-			rs=psmt.executeQuery();
-			while(rs.next()) {
-				if(id.equals(rs.getString(1))) {
-					result=false;
-					break;
-				}
-			psmt=con.prepareStatement(sql);
-			psmt.setString(1, id);
-			rs=psmt.executeQuery();
-			if(rs.next())
-			{
-				dto.setId(rs.getString(1));
-				dto.setPass(rs.getString(2));
-				dto.setNickname(rs.getString(3));
-				dto.setName(rs.getString(4));
-				dto.setTel(rs.getString(5));
-				dto.setRegidate(rs.getDate(6));
+   public boolean checkId(String id) {
+      boolean result=true;
+      memberDTO dto=new memberDTO();
+      try {
+         String sql = "select id from member where id=?";
+         psmt=con.prepareStatement(sql);
+         psmt.setString(1, id);
+         rs=psmt.executeQuery();
+         while(rs.next()) {
+            if(id.equals(rs.getString(1))) {
+               result=false;
+               break;
+            }
+         psmt=con.prepareStatement(sql);
+         psmt.setString(1, id);
+         rs=psmt.executeQuery();
+         if(rs.next())
+         {
+            dto.setId(rs.getString(1));
+            dto.setPass(rs.getString(2));
+            dto.setName(rs.getString(3));
+            dto.setTel(rs.getString(4));
+            dto.setNickname(rs.getString(5));
+            dto.setRegidate(rs.getDate(6));
 
-			}
-			}}catch(Exception e)
-		{
-			e.printStackTrace();
-		}
+         }
+         }}catch(Exception e)
+      {
+         e.printStackTrace();
+      }
 
-		return result;
+      return result;
 
-	}
+   }
 
-	public int setMemberDTO(memberDTO dto) {//회원가입
-		int result = 0;
-		try {
-			String sql = "INSERT INTO member"
-					+ " ( id, pass, name, tel, nickname, regidate)"
-					+ " VALUES ( "
-					+ " ?, ?, ?, ?, ?, sysdate)";
-			psmt = con.prepareStatement(sql);
-			psmt.setString(1, dto.getId());
-			psmt.setString(2, dto.getPass());
-			psmt.setString(3, dto.getNickname());
-			psmt.setString(4, dto.getName());
-			psmt.setString(5, dto.getTel());
+   public int setMemberDTO(memberDTO dto) {//회원가입
+      int result = 0;
+      try {
+         String sql = "INSERT INTO member"
+               + " ( id, pass, name, tel, nickname, regidate)"
+               + " VALUES ( "
+               + " ?, ?, ?, ?, ?, sysdate)";
+         psmt = con.prepareStatement(sql);
+         psmt.setString(1, dto.getId());
+         psmt.setString(2, dto.getPass());
+         psmt.setString(3, dto.getName());
+         psmt.setString(4, dto.getTel());
+         psmt.setString(5, dto.getNickname());
 
-			result = psmt.executeUpdate();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-		return result;
-	}
-	
-	public boolean idCheck(String id) {//아이디 중복체크
-		boolean result = false;
-		String sql = "select * from member";
-		try {
-			stmt= con.createStatement();
-			rs = stmt.executeQuery(sql);
-			while(rs.next()) {
-				if (rs.getString(1).equals(id)) {
-					result = true;
-				}
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
-		return result;
-	}
-	
-	public boolean nicCheck(String nic) {//닉네임 중복 체크
-		boolean result = false;
-		String sql = "select * from member";
-		try {
-			stmt= con.createStatement();
-			rs = stmt.executeQuery(sql);
-			while(rs.next()) {
-				if (rs.getString(3).equals(nic)) {
-					result = true;
-				}
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
-		return result;
-	}
-	
-	public int deleteMember(String id) { //회원삭제
-		int result = 0;
-		try {
-			String sql = "DELETE FROM member WHERE id=?";
-			psmt = con.prepareStatement(sql);
-			psmt.setString(1, id);
-			
-			result = psmt.executeUpdate();
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
-		return result;
-	}
-	
-	  public boolean updateMember(String memberId, String newPass, String newNickname, String newTel) {
-		  //회원정보 수정
-		    try {
-		      String sql = "UPDATE member SET pass = ?, nickname = ?, tel = ? WHERE id = ?";
-		      psmt = con.prepareStatement(sql);
-		      psmt.setString(1, newPass);
-		      psmt.setString(2, newNickname);
-		      psmt.setString(3, newTel);
-		      psmt.setString(4, memberId);
+         result = psmt.executeUpdate();
+      } catch(Exception e) {
+         e.printStackTrace();
+      }
+      return result;
+   }
+   
+   public boolean idCheck(String id) {//아이디 중복체크
+      boolean result = false;
+      String sql = "select * from member";
+      try {
+         stmt= con.createStatement();
+         rs = stmt.executeQuery(sql);
+         while(rs.next()) {
+            if (rs.getString(1).equals(id)) {
+               result = true;
+            }
+         }
+      } catch (SQLException e) {
+         e.printStackTrace();
+      }
+      
+      return result;
+   }
+   
+   public boolean nicCheck(String nic) {//닉네임 중복 체크
+      boolean result = false;
+      String sql = "select * from member";
+      try {
+         stmt= con.createStatement();
+         rs = stmt.executeQuery(sql);
+         while(rs.next()) {
+            if (rs.getString(5).equals(nic)) {
+               result = true;
+            }
+         }
+      } catch (SQLException e) {
+         e.printStackTrace();
+      }
+      
+      return result;
+   }
+   
+   public int deleteMember(String id) { //회원삭제
+      int result = 0;
+      try {
+         String sql = "DELETE FROM member WHERE id=?";
+         psmt = con.prepareStatement(sql);
+         psmt.setString(1, id);
+         
+         result = psmt.executeUpdate();
+      }catch(Exception e) {
+         e.printStackTrace();
+      }
+      return result;
+   }
+   
+     public boolean updateMember(String memberId, String newPass, String newNickname, String newTel) {
+        //회원정보 수정
+          try {
+            String sql = "UPDATE member SET pass = ?, nickname = ?, tel = ? WHERE id = ?";
+            psmt = con.prepareStatement(sql);
+            psmt.setString(1, newPass);
+            psmt.setString(2, newNickname);
+            psmt.setString(3, newTel);
+            psmt.setString(4, memberId);
 
-		      int rs = psmt.executeUpdate();
-		      psmt.close();
+            int rs = psmt.executeUpdate();
+            psmt.close();
 
-		      return rs > 0; // 업데이트된 행의 수가 1 이상이면 성공적으로 업데이트된 것으로 판단
-		    } catch (SQLException e) {
-		      e.printStackTrace();
-		    }
+            return rs > 0; // 업데이트된 행의 수가 1 이상이면 성공적으로 업데이트된 것으로 판단
+          } catch (SQLException e) {
+            e.printStackTrace();
+          }
 
-		    return false;
-		  }
-	  public List<memberDTO> listMember()
-	  { List<memberDTO> list=new ArrayList<memberDTO>();
-	  	String sql="select id,nickname,name,regidate from member";
-	  	try {
-	  		stmt=con.createStatement();
-	  		rs=stmt.executeQuery(sql);
-	  		while(rs.next()) {
-	  			memberDTO dto=new memberDTO();
-	  			dto.setId(rs.getString(1));
-	  			dto.setNickname(rs.getString(3));
-	  			dto.setName(rs.getString(4));
-	  			dto.setRegidate(rs.getDate(6));
-	  			list.add(dto);
-	  		}
-	  		
-	  	}catch(SQLException e) {
-	  		e.printStackTrace();
-	  	
-	  	}
-	  	return list;
-	  
-		  
-	  }
-	}
-
-
+          return false;
+        }
+     public List<memberDTO> listMember()
+     { List<memberDTO> list=new ArrayList<memberDTO>();
+        String sql="select id,nickname,name,regidate from member";
+        try {
+           stmt=con.createStatement();
+           rs=stmt.executeQuery(sql);
+           while(rs.next()) {
+              memberDTO dto=new memberDTO();
+              dto.setId(rs.getString(1));
+              dto.setNickname(rs.getString(2));
+              dto.setName(rs.getString(3));
+              dto.setRegidate(rs.getDate(4));
+              list.add(dto);
+           }
+           
+        }catch(SQLException e) {
+           e.printStackTrace();
+        
+        }
+        return list;
+     
+        
+     }
+   }
